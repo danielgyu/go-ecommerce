12da2877b0956b13f009aaaa811f751c1fa2e469
kungyu <hismayfly@gmail.com>
2021-10-22 00:30:30 +0900

    need to implement repository

--- pkg/products/handler.go
+++ pkg/products/handler.go
@@ -1,6 +1,10 @@
 package products
 
-import pb "github.com/danielgyu/go-ecommerce/internal/proto"
+import (
+	"context"
+
+	pb "github.com/danielgyu/go-ecommerce/internal/proto"
+)
 
 type productsHandler struct {
 	pb.UnimplementedProductServiceServer
@@ -8,4 +12,14 @@ type productsHandler struct {
 
 func NewProductHandler() *productsHandler {
 	return &productsHandler{}
+}
+
+func (h *productsHandler) GetProduct(ctx context.Context, in pb.GetProductRequest) (out pb.GetProductResponse, err error) {
+	productId := int(in.GetId())
+
+	return
+}
+
+func (h *productsHandler) GetProducts(ctx context.Context, in pb.GetProductsRequest) (out pb.GetProductsResponse, err error) {
+	return
 }
